63584026661ae83d44c3a20ee58b96c67a64b332
Ferdinand Lukito <devferdinandlukito@gmail.com>
2024-11-10 11:48:34 -0500

    Implementation
    Implemented Field class, created instance of Field class, added print function to show the field

--- main.js
+++ main.js
@@ -4,3 +4,29 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(twoDArray){
+        // validate 2-D array is passed in
+        if(!Array.isArray(twoDArray) || !twoDArray.every(row => Array.isArray(row))){
+            throw new Error('The input passed in constructor must be 2-dimensional array');
+        }
+        // if here then input is valid
+        this.data = twoDArray;
+    }
+
+    // print the elements in 2-D array
+    prettyPrint(){
+        this.data.forEach(row => console.log(row.join(" ")));
+    }
+}
+
+// create an instance of the Field class
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░']
+])
+
+// print myField
+myField.prettyPrint();
